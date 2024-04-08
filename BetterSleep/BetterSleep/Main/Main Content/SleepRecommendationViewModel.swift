4efff31af862dfd9c95cba03f8db4be67570197c
alyssa <alyssa.verasamy@gmail.com>
2024-04-08 11:33:56 -0400

    moved sleeprecommendation logic to viewmodel

--- BetterSleep/BetterSleep/Main/Main Content/SleepRecommendationViewModel.swift
+++ BetterSleep/BetterSleep/Main/Main Content/SleepRecommendationViewModel.swift
@@ -6,3 +6,45 @@
 //
 
 import Foundation
+import FirebaseFirestore
+import FirebaseAuth
+
+class SleepRecommendationViewModel: ObservableObject {
+    
+    //TODO: add publisher to avoid warnings updating these variables in function
+    @Published var preferences: UserPreferences = UserPreferences(antiBlueLightMode: false, disableStars: false)
+    @Published var recommendations: [Recommendation] = []
+    private var user: User = User(id: "", username: "", email: "", sleepHistory: [], recommendations: [], preferences: UserPreferences(antiBlueLightMode: false, disableStars: false), timeToSleep: nil, timetoWake: nil)
+    
+    init() {
+    }
+    
+    //TODO: move all database functions to FireDBHelper
+    func fetchUser() async {
+        
+        guard let userId = Auth.auth().currentUser?.uid else {
+            return
+        }
+        
+        print(#function, "attempting to log in user id \(userId)")
+        
+        let db = Firestore.firestore()
+        let docRef = db.collection("users").document(userId)
+        
+        do {
+            self.user = try await docRef.getDocument(as: User.self)
+            self.preferences = self.user.preferences
+            self.recommendations = self.user.recommendations
+            print("user: \(self.user)")
+        } catch {
+            print("error decoding user \(error)")
+        }
+        
+    }
+        
+
+}
+    
+    
+
+
